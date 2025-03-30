113a8ceaf38f861d18a0dce20dc77bf1135a52d8
Cleidson <cleidson.mac@outlook.com>
2025-03-30 14:12:19 +0100

    OK! I guess We have some sucess ussing the Agora DS, now i will find a way to use it on a simple layout on this project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const { AgoraTailwindConfig } = require("@ama-pt/agora-design-system");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  content: ["src/**/*.{ts,tsx}"],
+  theme: AgoraTailwindConfig.theme,
+  plugins: AgoraTailwindConfig.plugins,
+  safelist: AgoraTailwindConfig.safelist,
+  corePlugins: {
+    preflight: false,
   },
-  plugins: [],
-}
-
+};

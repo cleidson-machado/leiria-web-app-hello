87ed4140034d891bfe46167bee5d921b28e87b0e
Cleidson <cleidson.mac@outlook.com>
2025-04-01 15:15:12 +0100

    Add another basic layout to the Nav Bar

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -5,18 +5,132 @@ import { GridContainer } from "../GridContainer";
 export function MainNavBar() {
   return (
     <GridContainer className="bg-turquoise-dark">
-      <div className="flex items-center justify-between py-16">
-        <div className="flex items-center p-8 space-x-4 rounded">
+      <div className="flex items-center justify-between py-14">
+        {/* Left Section: Links */}
+        <nav className="flex items-center justify-between p-8 space-x-4 rounded">
+          <Link href="/" className="text-white hover:underline">
+            Home
+          </Link>
           <Image
             src="/space_image.png"
             width={30}
             height={40}
             alt="Logo Web Site Leiria"
           />
-          <Link href="/" className="text-white hover:underline">
-            Home
+          <Link href="/area-pessoal" className="text-white hover:underline">
+            Área Pessoal
           </Link>
-        </div>
+          <Image
+            src="/space_image.png"
+            width={15}
+            height={40}
+            alt="Logo Web Site Leiria"
+          />
+          <Link href="/agenda" className="text-white hover:underline">
+            Agenda
+          </Link>
+          <Image
+            src="/space_image.png"
+            width={15}
+            height={40}
+            alt="Logo Web Site Leiria"
+          />
+          <div className="relative group">
+            <button className="text-white hover:underline">
+              Balcão de Serviços
+            </button>
+            <div className="absolute hidden mt-2 text-black bg-white rounded shadow-lg group-hover:block">
+              <Link
+                href="/servico-1"
+                className="block px-4 py-2 hover:bg-gray-200"
+              >
+                Serviço 1
+              </Link>
+              <Link
+                href="/servico-2"
+                className="block px-4 py-2 hover:bg-gray-200"
+              >
+                Serviço 2
+              </Link>
+            </div>
+          </div>
+          <Image
+            src="/space_image.png"
+            width={15}
+            height={40}
+            alt="Logo Web Site Leiria"
+          />
+          <Link href="/contactos" className="text-white hover:underline">
+            Contactos
+          </Link>
+          <Image
+            src="/space_image.png"
+            width={15}
+            height={40}
+            alt="Logo Web Site Leiria"
+          />
+          <div className="relative group">
+            <button className="text-white hover:underline">
+              Portal de Acesso de Dados
+            </button>
+            <div className="absolute hidden mt-2 text-black bg-white rounded shadow-lg group-hover:block">
+              <Link
+                href="/dados-1"
+                className="block px-4 py-2 hover:bg-gray-200"
+              >
+                Dados 1
+              </Link>
+              <Link
+                href="/dados-2"
+                className="block px-4 py-2 hover:bg-gray-200"
+              >
+                Dados 2
+              </Link>
+            </div>
+          </div>
+          <Image
+            src="/space_image.png"
+            width={15}
+            height={40}
+            alt="Logo Web Site Leiria"
+          />
+          <div className="relative group">
+            <button className="text-white hover:underline">DSA</button>
+            <div className="absolute hidden mt-2 text-black bg-white rounded shadow-lg group-hover:block">
+              <Link href="/dsa-1" className="block px-4 py-2 hover:bg-gray-200">
+                DSA 1
+              </Link>
+              <Link href="/dsa-2" className="block px-4 py-2 hover:bg-gray-200">
+                DSA 2
+              </Link>
+            </div>
+          </div>
+          <Image
+            src="/space_image.png"
+            width={15}
+            height={40}
+            alt="Logo Web Site Leiria"
+          />
+          <div className="relative group">
+            <button className="text-white hover:underline">
+              Administração
+            </button>
+            <div className="absolute hidden mt-2 text-black bg-white rounded shadow-lg group-hover:block">
+              <Link
+                href="/admin-1"
+                className="block px-4 py-2 hover:bg-gray-200"
+              >
+                Administração 1
+              </Link>
+              <Link
+                href="/admin-2"
+                className="block px-4 py-2 hover:bg-gray-200"
+              >
+                Administração 2
+              </Link>
+            </div>
+          </div>
+        </nav>
       </div>
     </GridContainer>
   );

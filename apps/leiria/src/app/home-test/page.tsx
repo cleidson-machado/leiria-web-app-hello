0659e2fffe0be89c36494d68dc6e6454f0e15cf7
Cleidson <cleidson.mac@outlook.com>
2025-04-11 14:02:22 +0100

    Resolvido o Basico de como usar uma Rota no NextJs Mais novo

---
+++ apps/leiria/src/app/home-test/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@repo/ui";
+
+export default function Home() {
+  return (
+    <div>
+      <Button className="custom-class">Click Me</Button>
+    </div>
+  );
+}

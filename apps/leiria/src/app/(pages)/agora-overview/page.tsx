74bcf41bd5ea811ae7cd8b471983f8236db64559
Cleidson <cleidson.mac@outlook.com>
2025-04-11 17:12:47 +0100

    Add A base de Criação da Agora-OverView Page

---
+++ apps/leiria/src/app/(pages)/agora-overview/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styles from "./styles.module.css";
+
+export default function AgoraOverViewPage() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.heading}>Preview</h1>
+      <p className={styles.paragraph}>
+        Bem-vindos a Página de Demostração dos componentes compartilhados -
+        Agora Ds
+      </p>
+    </div>
+  );
+}

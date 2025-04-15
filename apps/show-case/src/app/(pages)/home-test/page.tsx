937cb347503b1cd0ff88efc8a0176e8b14985776
Cleidson <cleidson.mac@outlook.com>
2025-04-15 23:24:06 +0100

    Add Btn function for test

--- apps/show-case/src/app/(pages)/home-test/page.tsx
+++ apps/show-case/src/app/(pages)/home-test/page.tsx
@@ -3,9 +3,15 @@
 import { Button } from "@repo/ui";
 
 export default function Home() {
+  const handleClick = () => {
+    alert("Botão foi clicado!");
+  };
+
   return (
     <div>
-      <Button className="custom-class">Click Me</Button>
+      <Button className="custom-class" onClick={handleClick}>
+        Click Me
+      </Button>
     </div>
   );
 }
